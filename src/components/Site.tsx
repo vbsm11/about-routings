ae6b8e0f4803ad1355f88b88733f7404e3342751
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-05-22 14:45:32 +0300

    Rendering navbar depending on window width (custom hook useWindowSize!!!)

--- src/components/Site.tsx
+++ src/components/Site.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './Site.module.css'
 import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
 import {Page} from './pages/Page';
@@ -7,16 +7,39 @@ import {styled} from 'styled-components';
 
 
 export const Site = () => {
+
+    function useWindowSize() {
+        const [windowSize, setWindowSize] = useState(0);
+
+        useEffect(() => {
+
+            function handleResize() {
+                setWindowSize(window.innerWidth)
+            }
+
+            window.addEventListener('resize', handleResize);
+            handleResize();
+
+            return () => window.removeEventListener('resize', handleResize);
+        }, []);
+        return windowSize;
+    }
+
+    let size = useWindowSize()
+    console.log(size)
+
     return (
         <div>
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
 
-                <div className={styles.nav}>
-                    <NavWrapper><NavLink to="/page/0">Page 1</NavLink></NavWrapper>
-                    <NavWrapper><NavLink to="/page/1">Page 2</NavLink></NavWrapper>
-                    <NavWrapper><NavLink to="/page/2">Page 3</NavLink></NavWrapper>
-                </div>
+                {size > 600 &&
+                    <div className={styles.nav}>
+                        <NavWrapper><NavLink to="/page/0">Page 1</NavLink></NavWrapper>
+                        <NavWrapper><NavLink to="/page/1">Page 2</NavLink></NavWrapper>
+                        <NavWrapper><NavLink to="/page/2">Page 3</NavLink></NavWrapper>
+                    </div>
+                }
 
                 <div className={styles.content}>
                     <Routes>
